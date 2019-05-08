e94c99890f1c439678f8011e5a5e9e45861c316a
piaforastiere <pi.forastiere@gmail.com>
2019-05-08 18:31:27 +0200

    first commit

---
+++ precache-manifest.d1fc658ca653e11ca2d8c88455c57376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ff972c04cf756b8e94540c578a0892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d17daf771ae25f73ceb",
+    "url": "/static/css/main.a6a091b5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8d17daf771ae25f73ceb",
+    "url": "/static/js/main.eafebd0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
